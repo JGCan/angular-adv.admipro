7709a4c4b8d57b681051a62394692311e45db1e8
JGCan <jhgcandia@gmail.com>
2021-05-20 15:18:41 -0400

    Fin seccion 7

--- src/app/pages/pages.routing.ts
+++ src/app/pages/pages.routing.ts
@@ -5,6 +5,7 @@ import { PagesComponent } from './pages.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { ProgressComponent } from './progress/progress.component';
 import { Grafica1Component } from './grafica1/grafica1.component';
+import { AccountsSettingsComponent } from './accounts-settings/accounts-settings.component';
 
 const routes: Routes = [
     { 
@@ -13,6 +14,7 @@ const routes: Routes = [
           { path: '', component: DashboardComponent},
           { path: 'progress', component: ProgressComponent},
           { path: 'grafica1', component: Grafica1Component},
+          { path: 'account-settings', component: AccountsSettingsComponent},
           // { path: '', redirectTo: '/dashboard', pathMatch: 'full'},
         ]
       },
